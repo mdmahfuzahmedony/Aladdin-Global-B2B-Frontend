f0abad070dccadff2f03524be74308eee59bd1e1
mdmahfuzahmedony <Mdmahfuzahmedony@gmail.com>
2026-09-06 13:54:04 +0600

    [setup] project setup by forntend next.js

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,13 @@
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 };
 
-export default config;
+export default nextConfig;
